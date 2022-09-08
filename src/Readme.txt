caf5322628a5f55740224e43eb24932fa8352e81
[Gopi-Subedi] <[gopi.subedi09@gmail.com]>
2022-09-08 21:06:50 +0545

    player panel completed

---
+++ src/Readme.txt
@@ -0,0 +1,11 @@
+Every football fans are familiar with FIFA22, an EA Sports game that attempt to mimic real world football in digital gaming.
+DeFIFA is an online web application to review player details included in FIFA22.
+
+Technologies used:
+* Front-end tool: React.js, React-ApexChart, React-icons, BootStrap5, Sass (Sassy CSS)
+* Back-end tool: Java, Spring Boot
+* Database: SQL Server
+
+
+Special Credit:
+* Sofifa and team.
--- src/components/splashPage/splashPage.jsx
+++ src/components/splashPage/splashPage.jsx
@@ -1,32 +1,37 @@
 import React, { useContext, useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 import { Fetchdata } from "../../hooks/getData";
+import { TopPlayers } from "../dummyData/dummyList";
+import PlayerDetails from "./playerDetails";
 import "./splashPage.scss";
 
 export default function SplashPage() {
   const [loading, setLoading] = useState(true);
   const [playerList, setPlayerList] = useState([]);
+  const [selectedPlayer, setSelectedPlayer] = useState([]);
+  const [showDetails, setShowDetails] = useState(false);
 
   const columns = [
     {
-      name: "",
-      grow: 1,
-      center: true,
+      name: "NAME",
+      width: "200px",
+      // center: true,
       selector: (row) => {
         return (
           <>
-            <img src={row.photo} className="player-photo" />
+            <div className="player-details">
+              <div className="player-no">{row.jerseyNo}</div>
+              <div className="player-two">
+                <div className="player-name">{row.name}</div>
+                <div className="player-position">{row.position}</div>
+                <div className="player-nationality">{row.nationality}</div>
+                <div className="player-club">{row.club}</div>
+              </div>
+            </div>
           </>
         );
       },
     },
-    {
-      name: "NAME",
-      width: "130px",
-      // center: true,
-      selector: (row) => row.name,
-    },
-    // { name: "Date", grow: 0, center: true, selector: (row) => row.LeaveTitle },
     {
       name: "AGE",
       center: true,
@@ -74,19 +79,25 @@ export default function SplashPage() {
           console.log("Post Result =>", result);
           const postResult = result.playersList ? result.playersList : "";
           setPlayerList(postResult);
-          //   setOriginalList(postResult);
           setLoading(false);
         } else {
-          setPlayerList([]);
+          setPlayerList(TopPlayers);
           console.log("Fetch error!");
           setLoading(false);
         }
       })
       .catch((err) => {
-        setPlayerList([]);
+        setPlayerList(TopPlayers);
         setLoading(false);
       });
   }, []);
+
+  const handleRowClick = (row) => {
+    console.log(row);
+    setSelectedPlayer(row);
+    setShowDetails(true);
+  };
+
   return (
     <>
       <div className="splash-container ps-3 pe-3 pt-3">
@@ -113,10 +124,11 @@ export default function SplashPage() {
                 data={playerList}
                 pagination
                 fixedHeader
-                fixedHeaderScrollHeight="370px"
+                fixedHeaderScrollHeight="365px"
                 highlightOnHover
                 pointerOnHover
                 responsive
+                onRowClicked={handleRowClick}
                 dense
                 striped
               />
@@ -124,6 +136,12 @@ export default function SplashPage() {
           )}
         </div>
       </div>
+      {showDetails && selectedPlayer && (
+        <PlayerDetails
+          setShowDetails={setShowDetails}
+          player={selectedPlayer}
+        />
+      )}
     </>
   );
 }
